0fb0fbdb042a9647ba4aaa9e283c8baa5ffbbb14
alimehdi04 <alimehdinaqvi05@gmail.com>
2024-10-28 00:25:00 +0530

    updated vanilla to tailwind

---
+++ TextToSpeech/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'gray-900': '#1a202c',
+        'gray-800': '#2d3748',
+        'gray-700': '#4a5568',
+        'teal-500': '#38b2ac',
+        'teal-400': '#4fd1c5'
+      }
+    },
+  },
+  plugins: [],
+}
+
